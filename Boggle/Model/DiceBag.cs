88c0ebdd3bdadb6800e754f388764f114c5ac96d
Keith Heiner <keith.heiner@gmail.com>
2021-06-23 14:32:23 -0500

    Add Dice import

---
+++ Boggle/Model/DiceBag.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Boggle.Model
+{
+    public class DiceBag
+    {
+        private List<string> m_dice;
+
+        public DiceBag(IList<string> dice)
+        {
+            m_dice = dice.ToList();
+        }
+
+        private static readonly string _cubes =
+            @"aaafrs
+aaeeee
+aafirs
+adennn
+aeeeem
+aeegmu
+aegmnn
+afirsy
+bjkqxz
+ccenst
+ceiilt
+ceilpt
+ceipst
+ddhnot
+dhhlor
+dhlnor
+dhlnor
+eiiitt
+emottt
+ensssu
+fiprsy
+gorrvw
+iprrry
+nootuw
+ooottu";
+
+        public static DiceBag BasicDiceSet()
+        {
+            var cubesString = _cubes.Split('\n');
+            return new DiceBag(cubesString);
+        }
+    }
+}
---
+++ Boggle/RandomNumberGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Boggle
+{
+    public static class RandomNumberGenerator
+    {
+        public static Random Random = new Random();
+    }
+    
+    //TODO: Generate Random set of letters
+}
