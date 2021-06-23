381043baded5f40e6af1975552e15516b802911a
Keith Heiner <keith.heiner@gmail.com>
2021-06-23 16:37:09 -0500

    Working state for selection and submission

--- Boggle/MainWindow.xaml.cs
+++ Boggle/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using Boggle.Model;
@@ -15,10 +16,15 @@ namespace Boggle
 
         private TextBlock[,] m_diceTextBlock;
 
-        private Stack<(int hPos, int vPos)> m_selectedDice = new Stack<(int, int)>();
+        private List<(int hPos, int vPos)> m_selectedDice = new List<(int, int)>();
         private char[,] m_boardConfiguration;
-        
-        private HashSet<string> m_wordList = Dictionary.CreateDictionary(@"D:\DDocuments\PersonalProjects\Boggle\Boggle\Boggle\WordList.txt");
+        private string m_currentWord;
+        private List<string> m_scoredWords = new List<string>();
+        private List<List<(int, int)>> m_scoredPositions = new List<List<(int, int)>>();
+        private int m_currentScore = 0;
+
+        private HashSet<string> m_wordList =
+            Dictionary.CreateDictionary(@"D:\DDocuments\PersonalProjects\Boggle\Boggle\Boggle\WordList.txt");
 
         public MainWindow()
         {
@@ -70,22 +76,23 @@ namespace Boggle
                     var horizPos = i;
                     var vertPos = j;
                     m_diceTextBlock[i, j].MouseDown += (sender, args) =>
-                        HandleClickDie(horizPos, vertPos, m_diceTextBlock[horizPos, vertPos]);
+                        HandleClickDie(horizPos, vertPos);
                 }
             }
         }
 
-        private void HandleClickDie(int i, int j, TextBlock textBlock)
+        private void HandleClickDie(int i, int j)
         {
             if (m_selectedDice.Count > 0)
             {
-                var lastEntry = m_selectedDice.Peek();
+                var lastEntry = m_selectedDice.Last();
                 // Undo
                 if (lastEntry == (i, j))
                 {
-                    m_selectedDice.Pop();
-                    textBlock.FontWeight = FontWeights.Normal;
-                    UpdateCurrentWord();
+                    var toDeselect = m_selectedDice.Last();
+                    m_selectedDice.RemoveAt(m_selectedDice.Count - 1);
+                    UpdateViewSelection(toDeselect.hPos, toDeselect.vPos, false);
+                    UpdateCurrentWord(m_selectedDice);
                     return;
                 }
 
@@ -101,22 +108,27 @@ namespace Boggle
                 }
             }
 
-            m_selectedDice.Push((i, j));
-            textBlock.FontWeight = FontWeights.Bold;
-            UpdateCurrentWord();
+            m_selectedDice.Add((i, j));
+            UpdateViewSelection(i, j, true);
+            UpdateCurrentWord(m_selectedDice);
+        }
+
+        private void UpdateViewSelection(int i, int j, bool shouldHighlight)
+        {
+            m_diceTextBlock[i, j].FontWeight = shouldHighlight ? FontWeights.Bold : FontWeights.Normal;
         }
 
 
-        private void UpdateCurrentWord()
+        private void UpdateCurrentWord(List<(int hPos, int vPos)> selectedDice)
         {
-            string curWord = "";
-            foreach (var diePosition in m_selectedDice)
+            m_currentWord = "";
+            foreach (var diePosition in selectedDice)
             {
                 var curLetter = m_boardConfiguration[diePosition.hPos, diePosition.vPos].DiceView();
-                curWord += curLetter;
+                m_currentWord += curLetter;
             }
 
-            CurrentWord.Text = curWord;
+            CurrentWord.Text = m_currentWord;
         }
 
 
@@ -137,6 +149,74 @@ namespace Boggle
 
             // Start Timer
             // Initialize Scores
+        }
+
+        private static int ScoreWord(int wordLength)
+        {
+            if (wordLength <= 2)
+            {
+                return 0;
+            }
+
+            switch (wordLength)
+            {
+                case 3:
+                    return 1;
+                case 4:
+                    return 1;
+                case 5:
+                    return 2;
+                case 6:
+                    return 3;
+                case 7:
+                    return 5;
+                default:
+                    return 11;
+            }
+            
+        }
+
+        private void SubmitButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            //Check if Valid Word
+            var valid = m_wordList.Contains(m_currentWord);
+            if (!valid)
+            {
+                m_currentScore -= 2;
+                //TODO: Show that word is invalid
+            }
+            else
+            {
+                var positionSequence = m_selectedDice.ToList();
+                if (m_scoredPositions.Contains(positionSequence))
+                {
+                    //TODO: Highlight word that's been used.
+                    return;
+                }
+
+                //Score Word
+                var letterCount = m_selectedDice.Count;
+                var wordScore = ScoreWord(letterCount);
+                if (wordScore == 0)
+                {
+                    //TODO: Inform too few letters for word?
+                    // Invalidate Button if less than 3 letters.
+                    return;
+                }
+
+                m_currentScore += wordScore;
+                //Add Word to List
+                m_scoredWords.Add(m_currentWord);
+                // Reset Selection Stack and selection view
+                foreach (var selectedDie in m_selectedDice)
+                {
+                    UpdateViewSelection(selectedDie.hPos, selectedDie.vPos, false);
+                }
+                m_selectedDice.Clear();
+                UpdateCurrentWord(m_selectedDice);
+            }
+
+            // Update List of words, selection, score, currentWord visualization
         }
     }
 }
--- Boggle/Model/Dictionary.cs
+++ Boggle/Model/Dictionary.cs
@@ -5,14 +5,16 @@ namespace Boggle.Model
 {
     public class Dictionary
     {
-
         public static HashSet<string> CreateDictionary(string wordListPath)
         {
             var wordList = new HashSet<string>();
             using (var file = new StreamReader(wordListPath))
             {
-                var line = file.ReadLine();
-                wordList.Add(line);
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine().Trim();
+                    wordList.Add(line);
+                }
             }
 
             return wordList;
